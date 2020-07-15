7650fff1a2bcf45dc3546d108b840c5f2ab3799b
Thomas O'Gara <thomasogara12@gmail.com>
2020-07-15 11:48:45 +0000

    ⚡ Upgrade project storage and retrieval

--- portfolio/src/main/webapp/gallery-script.js
+++ portfolio/src/main/webapp/gallery-script.js
@@ -18,16 +18,31 @@ const addProjectsToDOM = async (json) => {
     json will always contain a top-level object which has a single member.
     this member will be an array of objects. these objects will have three properties:
     - title : a string containing the title of the project
-    - URL : a string which contains a URL linking to the blog post about
+    - URL : a string which contains a URL linking to the source code of
         the project
     - preview : a string which contains a URL linking to the preview image
         for this project
+    - description : a string which contains a short description of this project
+    - blog_link : a string which contains a URL linking to a blog post about
+        the project
   */
   const data = JSON.parse(json);
   data.projects.map(async (project) => {
-    const img = document.createElement("img");
+    const content = document.getElementById('content');
+    const link = document.createElement('a');
+    const header = document.createElement('h2');
+    const details = document.createElement('p');
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    content.appendChild(div);
+    div.appendChild(link);
+    div.appendChild(img);
+    link.appendChild(header);
+    div.appendChild(details);
+    link.href = project.URL;
+    header.innerText = project.title;
     img.src = project.preview;
-    document.getElementById('content').appendChild(img);
+    details.innerText = project.description;
   });
 }
 
@@ -35,7 +50,9 @@ const addProjectsToDOM = async (json) => {
 const main = () => {
   const window_onload_old = window.onload;
   window.onload = () => {
-    window_onload_old();
+    if (typeof(window_onload_old) === 'function'){
+      window_onload_old();
+    }
     loadProjects();
   };
 };
