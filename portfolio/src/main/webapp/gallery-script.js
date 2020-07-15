b0799734646db2f82eff8fcabe9b41d041cd2da8
Thomas O'Gara <thomasogara12@gmail.com>
2020-07-15 10:28:07 +0000

    💡🔨 Add comments describing the project.json file structure, and re-factoring the main method of gallery-script.js

--- portfolio/src/main/webapp/gallery-script.js
+++ portfolio/src/main/webapp/gallery-script.js
@@ -1,4 +1,3 @@
-
 /**
  * Load project details from local registry of projects (projects.json)
  */
@@ -10,14 +9,20 @@ const loadProjects = async() => {
   request.send(null);
 }
 
-/* Load all projects immediately */
-loadProjects();
-
 /**
  * Handler for the successful retrieval of project details
  * from the local project registry
  */
 const addProjectsToDOM = async (json) => {
+  /*
+    json will always contain a top-level object which has a single member.
+    this member will be an array of objects. these objects will have three properties:
+    - title : a string containing the title of the project
+    - URL : a string which contains a URL linking to the blog post about
+        the project
+    - preview : a string which contains a URL linking to the preview image
+        for this project
+  */
   const data = JSON.parse(json);
   data.projects.map(async (project) => {
     const img = document.createElement("img");
@@ -25,3 +30,14 @@ const addProjectsToDOM = async (json) => {
     document.getElementById('content').appendChild(img);
   });
 }
+
+/* Load all projects immediately */
+const main = () => {
+  const window_onload_old = window.onload;
+  window.onload = () => {
+    window_onload_old();
+    loadProjects();
+  };
+};
+
+main();
