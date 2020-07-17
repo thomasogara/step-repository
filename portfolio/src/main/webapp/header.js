0048c7e8c7632b763bbc6338ea5418db8190f5e0
Thomas O'Gara <thomasogara12@gmail.com>
2020-07-17 14:40:10 +0000

    🎨 Update header.js to reflect new header

--- portfolio/src/main/webapp/header.js
+++ portfolio/src/main/webapp/header.js
@@ -2,7 +2,7 @@ const fetch_header = async () => {
   const path = "header.html";
   const data = await fetch(path);
   const text = await data.text();
-  const header = document.getElementById('header');
+  const header = document.getElementById('navbar-list');
   header.innerHTML = text;
 }
 
