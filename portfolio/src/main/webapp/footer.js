c819cdc4a2ce620aa94c037229b1e5df99c76f43
Thomas O'Gara <thomasogara12@gmail.com>
2020-07-17 12:19:45 +0000

    ⚡ Add header and footer loading files

---
+++ portfolio/src/main/webapp/footer.js
@@ -0,0 +1,9 @@
+const fetch_footer = async () => {
+  const path = "footer.html";
+  const data = await fetch(path);
+  const text = await data.text();
+  const footer = document.getElementById('footer');
+  footer.innerHTML = text
+}
+
+fetch_footer();
