98b9dbd0e5ac55542ba4a00cfa20f454a76fdbdf
Thomas O'Gara <thomasogara12@gmail.com>
2020-07-23 09:32:49 +0000

    🔨 Add title support to Comment class

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -18,11 +18,13 @@ package com.google.sps.data;
 public final class Comment {
 
   private final long id;
+  private final String title;
   private final String text;
   private final long timestamp;
 
-  public Comment(long id, String text, long timestamp) {
+  public Comment(long id, String title, String text, long timestamp) {
     this.id = id;
+    this.title = title;
     this.text = text;
     this.timestamp = timestamp;
   }
