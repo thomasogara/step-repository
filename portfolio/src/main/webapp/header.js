f902e264f174516836c2b0d2fb02ff0a01483218
Thomas O'Gara <thomasogara12@gmail.com>
2020-07-31 11:48:56 +0000

    redesign header to use nav element

--- portfolio/src/main/webapp/header.js
+++ portfolio/src/main/webapp/header.js
@@ -2,7 +2,7 @@ const fetch_header = async () => {
   const path = "header.html";
   const data = await fetch(path);
   const text = await data.text();
-  const header = document.getElementById('header-navbar');
+  const header = document.getElementById('header');
   header.innerHTML = text;
 }
 
